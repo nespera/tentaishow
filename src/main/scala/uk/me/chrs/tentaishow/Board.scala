143f4dee3e2babf6e93eb0e6783f0b26441aeb52
chrisr <c@24.org.uk>
2018-12-15 18:21:11 +0000

    Make some basic game board classes

--- src/main/scala/uk/me/chrs/tentaishow/Board.scala
+++ src/main/scala/uk/me/chrs/tentaishow/Board.scala
@@ -1,15 +1,45 @@
 package uk.me.chrs.tentaishow
 
 /*
- Coordinates on the board run as follows:
+ Coordinates on the board run as follows
 
- (0,0) ..... (0,n)
+ (0,0) ..... (0,m)
   .           .
   .           .
- (n,0) ..... (n,n)
+ (n,0) ..... (n,m)
+
+ (0,0) being the top left of the board
+ (1,1) being being in the centre of the square in row 0, col 0
+ (1,2) being to the right of that square
 
  */
 
-class Board(val height: Int, val width: Int) {
+case class Board(height: Int, width: Int) {
 
+  def isInside(square: Square): Boolean = {
+    square.row >= 0 && square.row < height && square.col >= 0 && square.col < width
+  }
 }
+
+case class Square(row: Int, col: Int) {
+  def adjacent(coordinate: Coordinate) : Boolean = {
+     coordinate.rows().contains(row) && coordinate.cols().contains(col)
+  }
+}
+
+case class Coordinate(x: Int, y: Int) {
+
+  def rows(): Seq[Int] = {
+    if (x % 2 == 0) Seq(Math.floorDiv(x-2, 2), Math.floorDiv(x, 2)) else Seq(Math.floorDiv(x-1, 2))
+  }
+  def cols(): Seq[Int] = {
+    if (y % 2 == 0) Seq(Math.floorDiv(y-2, 2), Math.floorDiv(y, 2)) else Seq(Math.floorDiv(y-1, 2))
+  }
+}
+
+sealed trait Colour
+
+case object White extends Colour
+case object Black extends Colour
+
+case class Star(colour: Colour, coordinate: Coordinate)
--- src/test/scala/uk/me/chrs/tentaishow/BoardSpec.scala
+++ src/test/scala/uk/me/chrs/tentaishow/BoardSpec.scala
@@ -6,8 +6,33 @@ class BoardSpec extends Specification {
 
   "A board" should {
 
-    "do something" in {
-      new Board(1, 1) must not(beEqualTo(0))
+    "detect squares inside the board" in {
+      val board = Board(2, 6)
+      board.isInside(Square(-1,0)) must beFalse
+      board.isInside(Square(0,-1)) must beFalse
+      board.isInside(Square(0,0)) must beTrue
+      board.isInside(Square(1,5)) must beTrue
+      board.isInside(Square(1,6)) must beFalse
+      board.isInside(Square(2,5)) must beFalse
+    }
+  }
+
+  "A coordinate" should {
+
+    "know the rows adjacent to it" in {
+      Coordinate(0,0).rows() mustEqual Seq(-1, 0)
+      Coordinate(1,0).rows() mustEqual Seq(0)
+      Coordinate(2,0).rows() mustEqual Seq(0, 1)
+      Coordinate(3,0).rows() mustEqual Seq(1)
+      Coordinate(4,0).rows() mustEqual Seq(1, 2)
+    }
+
+    "know the columns adjacent to it" in {
+      Coordinate(0,0).cols() mustEqual Seq(-1, 0)
+      Coordinate(0,1).cols() mustEqual Seq(0)
+      Coordinate(0,2).cols() mustEqual Seq(0, 1)
+      Coordinate(0,3).cols() mustEqual Seq(1)
+      Coordinate(0,4).cols() mustEqual Seq(1, 2)
     }
   }
 

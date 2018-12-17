ed7f2d618a216bc3bed483d8656d9d7859eb10c8
chrisr <c@24.org.uk>
2018-12-17 21:20:36 +0000

    Add a method to print out the game state

--- src/main/scala/uk/me/chrs/tentaishow/Game.scala
+++ src/main/scala/uk/me/chrs/tentaishow/Game.scala
@@ -1,8 +1,33 @@
 package uk.me.chrs.tentaishow
 
-case class Game(board: Board, stars: Set[Star], state: Map[Square, Option[Star]])
+case class Game(board: Board, stars: Set[Star], state: Map[Square, Option[Star]]) {
 
-object Game {
+  private val POINT = "•"
+  private val HORIZ = "‒"
+  private val VERT  = "|"
+
+  def print(): Unit = {
+    for(r <- 0 until board.height) {
+      println(gridLine())
+      for (c <- 0 until board.width) {
+        printf(VERT)
+        val fill = state(Square(r, c)).map(star => star.name).getOrElse(" ")
+        printf("%2s", fill)
+      }
+      println(VERT)
+    }
+    println(gridLine())
+  }
+
+  private def gridLine(): String = {
+    (0 until board.width).map(i => POINT + HORIZ + HORIZ).mkString("") + POINT
+  }
+}
+
+object Game extends App {
+
+  val game = init(Board(2,2), Set())
+  game.print()
 
   def init(board: Board, stars: Set[Star]): Game = {
     stars.find(s => !board.contains(s.coordinate))
@@ -13,7 +38,7 @@ object Game {
       adjacentStars.size > 1
     }).foreach(s => throw new IllegalArgumentException("Initialized with multiple stars adjacent to a single square"))
 
-    val initState = board.squares.map(sq => (sq, Option.empty)).toMap
+    val initState = board.squares.map(sq => (sq, None)).toMap
     Game(board, stars, initState)
   }
 }

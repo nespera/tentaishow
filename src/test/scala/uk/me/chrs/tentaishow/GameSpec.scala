f71b49f740ef85ccc857da5332049c8418418717
chrisr <c@24.org.uk>
2018-12-30 19:58:09 +0000

    Added extra tests around game initialization

--- src/test/scala/uk/me/chrs/tentaishow/GameSpec.scala
+++ src/test/scala/uk/me/chrs/tentaishow/GameSpec.scala
@@ -6,10 +6,22 @@ class GameSpec extends Specification {
 
   "A game" should {
 
+    "cannot be initialized with an invalid board" in {
+      val board = Board(2, 2, Set(Star("a", White, Coordinate(0,0))))
+      board.validate() must throwAn[IllegalArgumentException]
+      Game.init(board) must throwAn[IllegalArgumentException]
+    }
+
+    "is initialized with empty state" in {
+      val game = Game.init(Board(2, 2, Set()))
+      game.state.keySet mustEqual game.board.squares.toSet
+      game.state.values.toSet mustEqual Set(None)
+    }
+
     "be convertible to string with empty squares" in {
       val game = Game.init(Board(2, 2, Set()))
       val asString = "·‒‒‒·‒‒‒·\n|   |   |\n·‒‒‒·‒‒‒·\n|   |   |\n·‒‒‒·‒‒‒·\n"
-      game.toString mustEqual  asString
+      game.toString mustEqual asString
     }
 
     "be convertible to string with filled squares" in {
